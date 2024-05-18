0235cbb6a6a9e170d684935937de37a6c4f6baf7
vivek <vivekshelke12jul@gmail.com>
2024-05-19 03:27:05 +0530

    cards

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card"
+
+function App() {
+ 
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+        <img className="card-img" src="https://imgs.search.brave.com/Bih0LtdLSuJHDJjKkbxUa7sRmcS9c-Qk9EB62WJ6sp0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/b3JiaXRtZWRpYS5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjMvMDYvQW5keS1Q/cm9maWxlLTYwMC5w/bmc" alt="andy image" />
+        <h1 className='card-title'>Jonny Smith</h1>
+        <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, eius?</p>
+    </div>
+  )
+}
+
+export default Card
